f6440b31eacd7ad7aca5ddc8b614acf98e28c4df
Smitha Kamath <72394054+smitha-2020@users.noreply.github.com>
2023-10-09 10:37:17 +0300

    Delete backend/src directory

--- backend/src/core/filters/models/http-exception-response.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface HttpExceptionResponse {
-  statusCode: number;
-  message: string;
-}
-export interface HttpCustomExceptionResponse extends HttpExceptionResponse {
-  timestamp: Date;
-  method: string;
-  path: string;
-}
